db64da341d899af074da2922a621c651fbfdbb35
unknown <ioakaras@yahoo.com>
2023-09-24 14:18:44 +0300

    Commit WeatherApp

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+// Functional component for rendering top buttons
+function TopButtons({setQuery}) {
+
+    const cities = [
+        {
+            id:1,
+            title: 'Athens'
+        },
+        {
+            id:2,
+            title: 'Thessaloniki'
+        },
+        {
+            id:3,
+            title: 'Florina'
+        },
+        {
+            id:4,
+            title: 'Corfu'
+        },
+        {
+            id:5,
+            title: 'Crete'
+        },
+    ]
+
+    return (
+        <div className="flex items-center justify-around my-6">
+          {cities.map((city) => (
+            <button
+              key={city.id}
+              className="text-white text-lg font-medium"
+              onClick={() => setQuery({ q: city.title })}
+            >
+              {city.title}
+            </button>
+          ))}
+        </div>
+      );
+    }
+    
+
+export default TopButtons;
---
+++ src/services/weatherService.js
@@ -0,0 +1,52 @@
+import { DateTime } from "luxon";
+
+// Define of the OpenWeatherMap API key and base URL
+const API_KEY = '23b5b493de48f6b644e1dff27799ebcf';
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// Function to fetch weather data from the OpenWeather API 
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + '/' + infoType)
+    url.search = new URLSearchParams({...searchParams, appid:API_KEY});
+
+    return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  // Extracting relevant properties
+    const {
+        coord: {lat, lon},
+        main: {temp, feels_like, temp_min, temp_max, humidity},
+        name,
+        dt,
+        sys: {country, sunrise, sunset},
+        weather,
+        wind: {speed}
+    } = data
+
+    const {main: details, icon} = weather[0]
+
+    return {lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, icon, country, details, sunrise, sunset, weather, speed}
+}
+// Function to get and format weather data asynchronously
+const getFormattedWeatherData = async (searchParams) => {
+    const formattedCurrentWeather = await getWeatherData(
+      "weather",
+      searchParams
+    ).then(formatCurrentWeather);
+  
+    const { lat, lon } = formattedCurrentWeather;
+  
+    return { ...formattedCurrentWeather};
+  };
+
+// Function to format Unix timestap to local time
+const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).
+setZone(zone).toFormat(format);
+
+// Function to generate an icon URL from a weather code
+const iconUrlFromCode = (code) => `https://openweathermap.org/img/wn/${code}@2x.png`
+
+export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode };
